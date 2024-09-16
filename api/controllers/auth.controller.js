6b60278fd9961bfa0db3b653a21b9955d5895acd
Vigneshwar <vigneshwarj1996@gmail.com>
2024-09-16 16:20:32 +0530

    signin & signup functionaly added

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import { errorHandler } from "../utils/error.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, password, email } = req.body;
@@ -11,5 +12,25 @@ export const signup = async (req, res, next) => {
     res.status(201).json("User created successfully");
   } catch (err) {
     next(errorHandler(550, "error from the fn."));
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found!"));
+    const validPassword = bcrypt.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Invalid Password!"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pwd, ...rest } = validUser._doc; // Can't use password alone as it is already used.
+    res
+      .cookie("access_token", token, {
+        httponly: true,
+      })
+      .status(200)
+      .json(rest);
+  } catch (err) {
+    next(err);
   }
 };
